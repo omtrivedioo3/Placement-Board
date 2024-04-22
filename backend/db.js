ffac9151f03e08ee2519cfac2fd13c149af15e50
Om Trivedi <omtrivedioo3@gmail.com>
2024-04-22 23:01:33 +0530

    last error solved

--- backend/db.js
+++ backend/db.js
@@ -1,23 +1,24 @@
 const mongoose = require("mongoose");
-const URL=process.env.MONGO_URL
+const URL = process.env.MONGO_URL;
 // con
-mongoose.connect(URL, {
+mongoose.connect(
+  "mongodb+srv://omtrivedioo3:78wXMxojl1rc0rSZ@cluster0.vmygbyn.mongodb.net/?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
+  }
+);
 //  password : 78wXMxojl1rc0rSZ
-
-
 
 var db = mongoose.connection;
 try {
-    db.on("error", console.error.bind(console, "Connecton error"));
-    db.once("open", function () {
-        console.log(URL)
-        console.log("mongoDB connected");
-    });
+  db.on("error", console.error.bind(console, "Connecton error"));
+  db.once("open", function () {
+    console.log(URL);
+    console.log("mongoDB connected");
+  });
 } catch (err) {
-    console.log(err);
+  console.log(err);
 }
 
 module.exports = db;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,81 +4,106 @@ const express = require("express");
 // const Router = require("router");
 const { mongo } = require("mongoose");
 // const router = Router();
-const router = express.Router()
+const router = express.Router();
 const bcrypt = require("bcrypt");
 
-let newuser = {}
+let newuser = {};
 let user;
 // User login request
 router.post("/login", async (req, res) => {
-    let success = false;
-    const { email, Enrollment_no, password } = req.body;
-    user = await User.findOne({ email: email });
-    console.log(user)
-    if (user) {
-        newuser.email = email;
-        const hashPassword = await bcrypt.compare(password, user.password);
-        if (Enrollment_no === user.Enrollment_no) {
-            if (hashPassword) {
-                res.send({
-                    message: "Login Successfull", user: user,
-                    success: true
-                });
-            } else {
-                success = false;
-                res.send({
-                    message: "Password didn't match",
-                    success: false
-                });
-            }
-           
-        }
-        else {
-            success = false;
-            res.send({
-                message: "Enrollment number didn't match",
-                success: false
-            });
-        }
-    } else {
+  let success = false;
+  const { email, Enrollment_no, password } = req.body;
+  user = await User.findOne({ email: email });
+  console.log(user);
+  if (user) {
+    newuser.email = email;
+    const hashPassword = await bcrypt.compare(password, user.password);
+    if (Enrollment_no === user.Enrollment_no) {
+      if (hashPassword) {
+        res.send({
+          message: "Login Successfull",
+          user: user,
+          success: true,
+        });
+      } else {
         success = false;
-        res.send({ message: "User not registered", success: false });
+        res.send({
+          message: "Password didn't match",
+          success: false,
+        });
+      }
+    } else {
+      success = false;
+      res.send({
+        message: "Enrollment number didn't match",
+        success: false,
+      });
     }
-    // }
+  } else {
+    success = false;
+    res.send({ message: "User not registered", success: false });
+  }
+  // }
 });
-
 
 // User register request
 router.post("/register", async (req, res) => {
-
-    const { name, number, email, Enrollment_no, password, tenthMarks, twelfthMarks, resumePdf, address, skills, CPI, clg_name, department, semester } = req.body;
-    let success = false;
-    try {
-        let user = await User.findOne({ email: email });
-        if (user) {
-            success = false;
-            res.send({
-                message: "User already registerd",
-                user: user,
-                success: success,
-            });
-        } else {
-            const user = new User({
-                name, number, email, Enrollment_no, password, tenthMarks, twelfthMarks, resumePdf, address, skills, CPI, clg_name, department, semester
-            });
-            const hashPassword = await bcrypt.hash(password, 10);
-            user.password = hashPassword;
-            const doc = await user.save();
-            console.log(doc);
-            success = true;
-            res.json({
-                message: "User registered Successfull, Please Login now...",
-                success: success,
-            });
-        }
-    } catch (err) {
-        success = false;
-        res.send(err);
+  const {
+    name,
+    number,
+    email,
+    Enrollment_no,
+    password,
+    tenthMarks,
+    twelfthMarks,
+    resumePdf,
+    address,
+    skills,
+    CPI,
+    clg_name,
+    department,
+    semester,
+  } = req.body;
+  let success = false;
+  try {
+    let user = await User.findOne({ email: email });
+    if (user) {
+      success = false;
+      res.send({
+        message: "User already registerd",
+        user: user,
+        success: success,
+      });
+    } else {
+      const user = new User({
+        name,
+        number,
+        email,
+        Enrollment_no,
+        password,
+        tenthMarks,
+        twelfthMarks,
+        resumePdf,
+        address,
+        skills,
+        CPI,
+        clg_name,
+        department,
+        semester,
+      });
+      const hashPassword = await bcrypt.hash(password, 10);
+      user.password = hashPassword;
+      const doc = await user.save();
+      // console.log(doc);
+      success = true;
+      res.json({
+        message: "User registered Successfull, Please Login now...",
+        success: success,
+      });
     }
+  } catch (err) {
+    success = false;
+    res.send(err);
+  }
 });
 exports.router = router;
